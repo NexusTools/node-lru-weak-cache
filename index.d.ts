b60447f10338a8de722e209fb6179b8d6da49413
Katelyn Slater <ktaeyln@gmail.com>
2018-03-21 08:40:28 -0400

    Simplified index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,9 @@
 /// <reference types="node" />
 
-module lru_weak_cache {
-  declare interface LRUWeakCacheGenerator {
-    (key: string, callback: (err: Error, value?: V) => void): void;
-  }
-  declare interface LRUWeakCache<V extends object> extends Map<string, V> {
-    generate(key: string, generator: LRUWeakCacheGenerator, callback: (err: Error, value?: V) => void): void;
-  }
-  declare interface LRUWeakCacheConstructor {
-    new (options: {timeout?: number,capacity?: number,retimeOnAccess?:boolean}): LRUWeakCache;
-  }
-  declare var LRUWeakCache: LRUWeakCacheConstructor;
-}
 module "lru-weak-cache" {
-  export = lru_weak_cache.LRUWeakCacheConstructor;
+  declare class LRUWeakCache<V extends object> extends Map<string, V> {
+    constructor(options: {timeout?:number,capacity?:number,retimeOnAccess?:boolean});
+    generate(key: string, generator: (key: string, callback: (err: Error, value?: V) => void) => void, callback: (err: Error, value?: V) => void): void;
+  }
+  export = LRUWeakCache;
 }
