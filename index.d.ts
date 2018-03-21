f9a1aebbdfa48256e6d1921e8dfd05e89fd6b657
Katelyn Slater <ktaeyln@gmail.com>
2018-03-21 08:32:03 -0400

    Fixed typings

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,17 @@
-declare const _default: {
-    new <V extends object>(options?: {
-        timeout?: number;
-        capacity?: number;
-        retimeOnAccess?: boolean;
-    }): {
-        accesses: {
-            [index: string]: number;
-        };
-        timeouts: {
-            [index: string]: number;
-        };
-        destructors: {
-            [index: string]: Function;
-        };
-        generateQueue: {
-            [index: string]: ((err: Error, value?: V) => void)[];
-        };
-        retimeOnAccess: boolean;
-        capacity: number;
-        timeout: number;
-        clear(): void;
-        delete(key: string): boolean;
-        makeDestruct(key: string): () => void;
-        set(key: string, value: V): any;
-        get(key: string): V;
-        forEach(callbackfn: (value: V, key: string, map: Map<string, V>) => void, thisArg?: any): void;
-        generate(key: string, generator: (key: string, callback: (err: Error, value?: V) => void) => void, callback: (err: Error, value?: V) => void): void;
-        entries(): IterableIterator<[string, V]>;
-        values(): IterableIterator<V>;
-        has(key: string): boolean;
-        readonly size: number;
-        [Symbol.iterator](): IterableIterator<[string, V]>;
-        keys(): IterableIterator<string>;
-        readonly [Symbol.toStringTag]: "Map";
-    };
-};
-export = _default;
+/// <reference types="node" />
+
+module lru_weak_cache {
+  declare interface LRUWeakCacheGenerator {
+    (key: string, callback: (err: Error, value?: V) => void): void;
+  }
+  declare interface LRUWeakCache<V extends object> extends Map<string, V> {
+    generate(key: string, generator: LRUWeakCacheGenerator, callback: (err: Error, value?: V) => void): void;
+  }
+  declare interface LRUWeakCacheConstructor {
+    new (options: {timeout?: number,capacity?: number,retimeOnAccess?:boolean}): LRUWeakCache;
+  }
+  declare var LRUWeakCache: LRUWeakCacheConstructor;
+}
+module "lru-weak-cache" {
+  export = lru_weak_cache.LRUWeakCacheConstructor;
+}
